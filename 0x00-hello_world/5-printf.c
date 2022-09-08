acd24c78385c088efc77e1b7b159036b3475ae69
leonrichcy <slidetechn@gmail.com>
2022-09-08 12:34:07 -0700

    hot fix

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,9 @@
-File  12 lines (11 sloc)  185 Bytes
-
 #include <stdio.h>
 
 /**
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (Success)
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
