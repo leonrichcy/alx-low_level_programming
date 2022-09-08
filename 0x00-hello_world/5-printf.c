d5ef04e1894d48e6e39f66f80189b105e88ad457
leonrichcy <slidetechn@gmail.com>
2022-09-08 12:24:51 -0700

    hot fix

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+File  12 lines (11 sloc)  185 Bytes
+
+#include <stdio.h>
+
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ */
+int main(void)
+{
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+		return (0);
+}
