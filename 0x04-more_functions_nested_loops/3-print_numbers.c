74d163a2045bc8aa6f09ca3da569faee7bb4a7d5
leonrichcy <slidetechn@gmail.com>
2022-09-15 05:41:49 -0700

    Hot fix

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main -> a program that prints the alphabets in lowercase,
+ * followed by a new line
+ * Return: Always 0
+ */
+ int main(void)
+ {
+
+	char ch;
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch != 'q' && ch != 'e')
+		{
+			putchar(ch);
+		}
+		putchar(10);
+		return (0);
+	}
+ }
+Footer
+© 2022 GitHub, Inc.
